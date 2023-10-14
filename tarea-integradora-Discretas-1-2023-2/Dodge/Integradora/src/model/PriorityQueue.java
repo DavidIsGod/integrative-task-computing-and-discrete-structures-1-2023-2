2b46c5f985c5a8513e0fe89a10596bb7b33ce8de
DavidIsGod <troyaalejandro4@gmail.com>
2023-10-14 12:15:24 -0500

    Priority Queue Implementada

---
+++ tarea-integradora-Discretas-1-2023-2/Dodge/Integradora/src/model/PriorityQueue.java
@@ -0,0 +1,29 @@
+package model;
+
+public class PriorityQueue implements IPriorityQueue{
+
+    private Nodo<T> first;
+
+    private Nodo<T> last;
+
+    private int size = 0;
+
+    public PriorityQueue() {
+
+    }
+
+    public boolean enqueue(T value) {
+        Nodo<T> nuevoNodo = new Nodo<>(value);
+        boolean flag = false;
+        if (first == null) {
+            first = nuevoNodo;
+            last = nuevoNodo;
+        } else {
+            last.setNext(nuevoNodo);
+            last = nuevoNodo;
+        }
+        size++;
+        return flag;
+
+    }
+}
